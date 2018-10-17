255b5095636ac43bbdf45cc794869f06db9ba47c
Bhashitha Hemantha <bhashithahemantha@gmail.com>
2018-10-17 21:45:47 +0530

    added VCs for screens

--- swoosh/swoosh/Controller/WelcomeVC.swift
+++ swoosh/swoosh/Controller/WelcomeVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeVC: UIViewController {
 
     @IBOutlet weak var backgroudImage: UIImageView!
     @IBOutlet weak var swooshLabel: UIImageView!
